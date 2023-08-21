c5509443af44d77ad59b455e93f3b7313cf10bc8
zeenee <dbwls982@gmail.com>
2023-08-21 18:13:42 +0900

    :truck: Edit: slider 관련 로직 event.js 파일로 이동

--- assets/js/event.js
+++ assets/js/event.js
@@ -1,14 +1,37 @@
-const $btnQue = document.querySelectorAll(".btn-que");
+const $btnQue = document.querySelectorAll('.btn-que');
 
 // tutorial 로딩
 $btnQue.forEach((element) => {
-    element.addEventListener("click", function (e) {
-        document.getElementById("t" + PAGE_NAME).classList.remove("active");
+    element.addEventListener('click', function (e) {
+        document.getElementById('t' + PAGE_NAME).classList.remove('active');
         PAGE_NAME = e.target.id.slice(1);
-        document.getElementById("t" + PAGE_NAME).classList.add("active");
+        document.getElementById('t' + PAGE_NAME).classList.add('active');
         history.pushState(null, PAGE_NAME, `?page=${PAGE_NAME}`);
         // // 문제 이동 시 에러 메시지 초기화
         // document.getElementById("result_desc").textContent = "";
         render(); // parser에 문제 렌더링
+    });
+});
+
+// input range 스타일 적용을 위한 코드
+const sliders = document.querySelectorAll('.slider');
+
+const setSliderTrack = (element) => {
+    const value = element.value;
+    const max = parseInt(element.max);
+    const min = parseInt(element.min);
+    const width = ((value - min) / (max - min)) * 100;
+    const color = getComputedStyle(element).color;
+
+    element.style.background = `linear-gradient(to right, ${color} 0%, ${color} ${width}%, #d9dbe0 ${width}%, #d9dbe0 100%)`;
+};
+
+sliders.forEach((slider) => {
+    setSliderTrack(slider);
+
+    slider.addEventListener('input', (e) => {
+        if (e.target.tagName == 'INPUT') {
+            setSliderTrack(e.target);
+        }
     });
 });

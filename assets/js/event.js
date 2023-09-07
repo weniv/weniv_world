68092c2106b090d852661c2bd1e832df6f67b4e2
zeenee <dbwls982@gmail.com>
2023-09-07 12:11:55 +0900

    :memo: Edit: input range 값 변경 로직 수정

--- assets/js/event.js
+++ assets/js/event.js
@@ -15,6 +15,7 @@ $btnQue.forEach((element) => {
 
 // input range 스타일 적용을 위한 코드
 const sliders = document.querySelectorAll('.slider');
+const rangeValueText = document.querySelectorAll('.slider + strong');
 
 const setSliderTrack = (element) => {
     const value = element.value;
@@ -26,13 +27,23 @@ const setSliderTrack = (element) => {
     element.style.background = `linear-gradient(to right, ${color} 0%, ${color} ${width}%, #d9dbe0 ${width}%, #d9dbe0 100%)`;
 };
 
+const observer = new MutationObserver((mutationsList) => {
+    mutationsList.forEach((mutation) => {
+        if (mutation.type === 'childList') {
+            const slider = mutation.target.previousElementSibling;
+            setSliderTrack(slider);
+        }
+    });
+});
+
 sliders.forEach((slider) => {
     setSliderTrack(slider);
+});
 
-    slider.addEventListener('input', (e) => {
-        if (e.target.tagName == 'INPUT') {
-            setSliderTrack(e.target);
-        }
+rangeValueText.forEach((target) => {
+    observer.observe(target, {
+        childList: true,
+        characterData: true,
     });
 });
 
@@ -67,4 +78,50 @@ darkModeButton.addEventListener('click', () => {
         document.documentElement.setAttribute('color-theme', 'dark');
         darkModeButton.classList.add('active');
     }
+});
+
+// world 메뉴 - 버튼 이벤트 추가
+const worldMenu = document.querySelector('.world-menu');
+
+worldMenu.addEventListener('click', (e) => {
+    const target = e.target;
+    if (target.classList.contains('btn-toggle')) {
+        target.classList.toggle('active');
+    }
+});
+
+// modal
+const wallButton = document.querySelector('.btn-wall');
+
+const createModal = (type, className, contents) => {
+    const modalDiv = document.createElement('div');
+    modalDiv.setAttribute('class', `${type}-modal ${className}`);
+
+    modalDiv.innerHTML += contents;
+
+    return modalDiv;
+};
+
+wallButton.addEventListener('click', () => {
+    const contents = `
+        <div class="input-wrap">
+            <input type="radio" name="wall-type" id="wall" value="wall" checked>
+            <label for="wall">wall(기본): orange</label>
+        </div>
+        <div class="input-wrap">
+            <input type="radio" name="wall-type" id="door" value="door">
+            <label for="door">door: sienna</label>
+        </div>
+        <div class="input-wrap">
+            <input type="radio" name="wall-type" id="fence" value="fence">
+            <label for="fence">fence: seagreen</label>
+        </div>
+        <div class="input-wrap">
+            <input type="radio" name="wall-type" id="delete" value="delete">
+            <label for="delete">delete</label>
+        </div>
+    `;
+
+    const modalElement = createModal('controller', 'wall-type', contents);
+    wallButton.parentNode.append(modalElement);
 });

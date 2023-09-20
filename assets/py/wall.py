2fc46183c431385e80997c785869fcbfea0c7834
baekg <baekg.dev@gmail.com>
2023-09-20 20:00:19 +0900

    :sparkles: Feat: 벽 데이터 구조 변경 및 초기화, 하이라이팅 기능

--- assets/py/wall.py
+++ assets/py/wall.py
@@ -1,50 +1,60 @@
 import js
-from coordinate import map_data, map_size, border_size
-
+from coordinate import map_data, map_size, border_size, wall_type
+         
 class Wall:
-    def __init__(self, wall_data):
-        self.wall_data=wall_data
-        
-        # 벽의 정보를 담는 컨테이너
-        container = js.document.createElement('div')
-        container.setAttribute('class', 'wall-container')
-        self.container = container
+    def __init__(self):
+      self.width = map_data['width']
+      self.height = map_data['height']
+      self.wall_data = {(0,0.5):'wall', (0, 1.5):'',(3,0.5):'fence'}
+      self.initWallDate()
+    
+      
+          
+    def initWallDate(self):
+      wall_data = {}
+      
+      x = 0
+      while x <= self.height - 1:
+          if x == int(x):  # x가 정수일 때
+              y = 0.5
+              while y <= self.width - 1:
+                  wall_data[(x, y)]=''
+                  y += 1
+          else:  # x가 정수가 아닐 때
+              y = 0
+              while y <= self.width - 1:
+                  wall_data[(x, y)]=''
+                  y += 1
+          x += 0.5
+          
+      self.wall_data = wall_data
+      print(f'wall_data: {self.wall_data}')
+      
       
     def drawWall(self):
-        # 초기 wall_data를 출력하는 함수
-        container = self.container
-        for type in self.wall_data.keys():
-          for (x,y) in self.wall_data[type]:
-            
-            if (x<0 or x>2*map_data["width"] or y<0 or y>2*map_data["height"]):
-              continue
-            self.addWall(type,(x,y))
-            
-    def addWall(self,type, pos):
-      x, y = pos
+      box_size = map_size + border_size*2
+      container = js.document.createElement('div')
+      container.setAttribute('class', 'wall-container')
       
-      # 상자 기준 크기
-      box_size = map_size+border_size*2
-      
-      wall = js.document.createElement('div')
-      wall.setAttribute('class','wall')
-      
-      if (isinstance(x, int)): # 세로
-        wall.setAttribute('data-direction','portrait')
-      else: # 가로
-        wall.setAttribute('data-direction','landscape')
-      
-      wall.style.top=f'{(x+0.5)*box_size}px'
-      wall.style.left=f'{(y+0.5)*box_size+1}px'
-      
-      wall.setAttribute('data-type',type)
-      self.container.appendChild(wall)
-
-         
-    def resetWall(self):
-        self.container.innerHTML=""
-        self.wall_data={'wall': [], 'door': [], 'fence':[]}
-        self.drawWall()
+      for (x, y) in self.wall_data.keys():
+        wall = js.document.createElement('div')
+        wall.setAttribute('class', 'wall')
+        wall.setAttribute('data-type', self.wall_data[(x,y)])
+        wall.style.top=f'{(x+0.5)*box_size}px'
+        wall.style.left=f'{(y+0.5)*box_size+1}px'
+        if(int(x)==x):
+            print('portrait', x, y,)  
+            wall.setAttribute('data-direction', 'portrait')
+        else:
+            print('landscape',x,y)
+            wall.setAttribute('data-direction', 'landscape')
+        container.appendChild(wall)
+      return container
           
-          
-            
+    def resizeWall(self):
+      '''
+      map의 크기가 변경될 때, wall_data를 갱신합니다.
+      '''
+      pass
+      
+      

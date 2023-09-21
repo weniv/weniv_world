77564353044b7fe817d3f508c176c64a32235aa3
baekg <baekg.dev@gmail.com>
2023-09-21 09:25:08 +0900

    :sparkles: Feat: map 크기 변경 시, 벽 데이터 갱신

--- assets/py/wall.py
+++ assets/py/wall.py
@@ -1,26 +1,25 @@
 import js
-from coordinate import map_data, map_size, border_size, wall_type
+from coordinate import map_data, map_size, wall_data, border_size, wall_type
          
 class Wall:
     def __init__(self):
       self.width = map_data['width']
       self.height = map_data['height']
-      self.wall_data = {}
-      self.initWallDate()
+      self.wall_data = self.initWallDate(self.width, self.height)
     
-    def initWallDate(self):
+    def initWallDate(self, width, height):
       wall_data = {}
       
       x = 0
-      while x <= self.height - 1:
+      while x <= height - 1:
           if x == int(x):  # x가 정수일 때
               y = 0.5
-              while y <= self.width - 1:
+              while y <= width - 1:
                   wall_data[(x, y)]=''
                   y += 1
           else:  # x가 정수가 아닐 때
               y = 0
-              while y <= self.width - 1:
+              while y <= width - 1:
                   wall_data[(x, y)]=''
                   y += 1
           x += 0.5
@@ -28,8 +27,7 @@ class Wall:
       wall_data[(0, 0.5)] = 'wall'
       wall_data[(0, 1.5)] = 'door'
       wall_data[(0, 2.5)] = 'fence'
-      self.wall_data = wall_data
-      
+      return wall_data
       
     def drawWall(self):
       box_size = map_size + border_size*2
@@ -49,8 +47,6 @@ class Wall:
         else:
             wall.setAttribute('data-direction', 'landscape')
         container.appendChild(wall)
-        
-        
       return container
     
     def changeWallType(self, event):
@@ -65,10 +61,21 @@ class Wall:
         event.target.style.outline = '1px solid red'
         
         
-    def resizeWall(self):
+    def resizeWall(self, width, height):
       '''
       map의 크기가 변경될 때, wall_data를 갱신합니다.
       '''
-      pass
+      # 새로운 wall_data를 생성
+      new_wall_data = self.initWallDate(width, height)
+      for key in new_wall_data.keys():
+        if key in self.wall_data.keys():
+          new_wall_data[key] = self.wall_data[key]
+          
+      self.width = width
+      self.height = height
+      self.wall_data = new_wall_data
+      wall_data = new_wall_data # 전역변수 wall_data 갱신
+      
+      return self.drawWall()
       
       

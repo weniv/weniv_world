350a04f1803f4d40bd5d1b2a7d20f3a566dc1abe
chimaek <pipiru100@gmail.com>
2023-09-25 11:55:56 +0900

    ✨ Feat: black formmater 적용 및 webpack.config.js 구성 및 적용
    Signed-off-by: chimaek <pipiru100@gmail.com>

--- assets/py/built_in_functions.py
+++ assets/py/built_in_functions.py
@@ -16,118 +16,128 @@ from item import Item
 #     result = output.value + result
 #     output.value = result
 
+
 def print(*args):
-    '''
+    """
     html 문서 내 출력
-    '''
-    output = js.document.getElementById('output')
-    result = ''
+    """
+    output = js.document.getElementById("output")
+    result = ""
     for arg in args:
         result += str(arg)
 
     if output:
-        paragraph = js.document.createElement('p')
+        paragraph = js.document.createElement("p")
         paragraph.innerHTML = result
-        paragraph.classList.add('output-item')
+        paragraph.classList.add("output-item")
         output.appendChild(paragraph)
     else:
         js.console.log(result)
 
-def say(text='', character=None, speech_time=5000):
-    '''
+
+def say(text="", character=None, speech_time=5000):
+    """
     charecter의 말풍선에 출력
-    '''
+    """
     if character != None:
         character.say(text)
     else:
-        if character_data[0]['character_obj'] != None:
-            character_data[0]['character_obj'].say(text, speech_time)
+        if character_data[0]["character_obj"] != None:
+            character_data[0]["character_obj"].say(text, speech_time)
         else:
-            print('캐릭터가 없습니다.')
+            print("캐릭터가 없습니다.")
+
 
 def directions(character=None):
-    '''
+    """
     character의 방향을 right, left, top, bottom으로 반환
-    '''
-    d = {0:'right', 1:'top', 2:'left', 3:'bottom'}
+    """
+    d = {0: "right", 1: "top", 2: "left", 3: "bottom"}
     if character != None:
-        # TODO: 0번째가 아니라 순회 돌면서 self.name으로 찾아서 directions 반환 
+        # TODO: 0번째가 아니라 순회 돌면서 self.name으로 찾아서 directions 반환
         # return d[character.directions] # self.directions가 제대로 반영 안되어 있음
-        return d[character_data[0]['directions']]
+        return d[character_data[0]["directions"]]
     else:
-        if character_data[0]['character_obj'] != None:
+        if character_data[0]["character_obj"] != None:
             # return d[character_data[0]['character_obj'].directions] # self.directions가 제대로 반영 안되어 있음
-            return d[character_data[0]['directions']]
+            return d[character_data[0]["directions"]]
         else:
-            print('캐릭터가 없습니다.')
+            print("캐릭터가 없습니다.")
             return None
-        
+
+
 def show_item(character=None):
-    '''
+    """
     character가 가지고 있는 아이템을 보여주는 함수
-    '''
+    """
     if character != None:
-        # TODO: 0번째가 아니라 순회 돌면서 self.name으로 찾아서 items 반환 
-        return character_data[0]['items']
+        # TODO: 0번째가 아니라 순회 돌면서 self.name으로 찾아서 items 반환
+        return character_data[0]["items"]
     else:
-        if character_data[0]['character_obj'] != None:
+        if character_data[0]["character_obj"] != None:
             # return d[character_data[0]['character_obj'].directions] # self.directions가 제대로 반영 안되어 있음
-            return character_data[0]['items']
+            return character_data[0]["items"]
         else:
-            print('캐릭터가 없습니다.')
+            print("캐릭터가 없습니다.")
             return None
+
 
 def set_item(x, y, name, count=1, description={}, character=None):
     if not (isinstance(x, int) and isinstance(y, int)):
-        js.alert('좌표는 정수로 입력해야 합니다.')
-        print(f'{x}, {y} error.TypeError: Position must be integer')
+        js.alert("좌표는 정수로 입력해야 합니다.")
+        print(f"{x}, {y} error.TypeError: Position must be integer")
         return None
-       
-    if not (0<=x<map_data['height'] and 0<=y<map_data['width']):
-        js.alert('월드를 벗어나서 아이템을 추가할 수 없습니다.')
-        print('error.OutOfWorld: out of world')
+
+    if not (0 <= x < map_data["height"] and 0 <= y < map_data["width"]):
+        js.alert("월드를 벗어나서 아이템을 추가할 수 없습니다.")
+        print("error.OutOfWorld: out of world")
         return None
 
     item = Item(x, y, name, count, description)
     item.draw()
-    if character_data[0]['character_obj'] != None:
-            character_data[0]['character_obj'].pick()
+    if character_data[0]["character_obj"] != None:
+        character_data[0]["character_obj"].pick()
+
 
 def move(character=None):
     if character != None:
         character.move()
     else:
-        if character_data[0]['character_obj'] != None:
-            character_data[0]['character_obj'].move()
+        if character_data[0]["character_obj"] != None:
+            character_data[0]["character_obj"].move()
         else:
-            print('캐릭터가 없습니다.')
+            print("캐릭터가 없습니다.")
+
 
 def turn_left(character=None):
     if character != None:
         character.turn_left()
     else:
-        if character_data[0]['character_obj'] != None:
-            character_data[0]['character_obj'].turn_left()
+        if character_data[0]["character_obj"] != None:
+            character_data[0]["character_obj"].turn_left()
         else:
-            print('캐릭터가 없습니다.')
+            print("캐릭터가 없습니다.")
+
 
 def pick(character=None):
     if character != None:
         character.pick()
     else:
-        if character_data[0]['character_obj'] != None:
-            character_data[0]['character_obj'].pick()
+        if character_data[0]["character_obj"] != None:
+            character_data[0]["character_obj"].pick()
         else:
-            print('캐릭터가 없습니다.')
+            print("캐릭터가 없습니다.")
+
 
 def put(character=None):
     if character != None:
         character.put()
     else:
-        if character_data[0]['character_obj'] != None:
-            character_data[0]['character_obj'].put()
+        if character_data[0]["character_obj"] != None:
+            character_data[0]["character_obj"].put()
         else:
-            print('캐릭터가 없습니다.')
+            print("캐릭터가 없습니다.")
+
 
 def repeat(count, f):
     if isinstance(count, int) == True:
@@ -137,48 +147,53 @@ def repeat(count, f):
         for i in range(0, f):
             count()
 
+
 def front_is_clear(character=None):
     if character != None:
-        print(f'character not none')
+        print(f"character not none")
         character.front_is_clear()
     else:
-        if character_data[0]['character_obj'] != None:
-            character_data[0]['character_obj'].front_is_clear()
+        if character_data[0]["character_obj"] != None:
+            character_data[0]["character_obj"].front_is_clear()
         else:
-            print('캐릭터가 없습니다.')
+            print("캐릭터가 없습니다.")
+
 
 def left_is_clear(character=None):
     if character != None:
         character.left_is_clear()
     else:
-        if character_data[0]['character_obj'] != None:
-            character_data[0]['character_obj'].left_is_clear()
+        if character_data[0]["character_obj"] != None:
+            character_data[0]["character_obj"].left_is_clear()
         else:
-            print('캐릭터가 없습니다.')
-            
+            print("캐릭터가 없습니다.")
+
+
 def right_is_clear(character=None):
     if character != None:
         character.right_is_clear()
     else:
-        if character_data[0]['character_obj'] != None:
-            character_data[0]['character_obj'].right_is_clear()
+        if character_data[0]["character_obj"] != None:
+            character_data[0]["character_obj"].right_is_clear()
         else:
-            print('캐릭터가 없습니다.')
-            
+            print("캐릭터가 없습니다.")
+
+
 def back_is_clear(character=None):
     if character != None:
         character.back_is_clear()
     else:
-        if character_data[0]['character_obj'] != None:
-            character_data[0]['character_obj'].back_is_clear()
+        if character_data[0]["character_obj"] != None:
+            character_data[0]["character_obj"].back_is_clear()
         else:
-            print('캐릭터가 없습니다.')
+            print("캐릭터가 없습니다.")
+
 
 def attack(character=None):
     if character != None:
         character.attack()
     else:
-        if character_data[0]['character_obj'] != None:
-            character_data[0]['character_obj'].attack()
+        if character_data[0]["character_obj"] != None:
+            character_data[0]["character_obj"].attack()
         else:
-            print('캐릭터가 없습니다.')
+            print("캐릭터가 없습니다.")
--- assets/py/map.py
+++ assets/py/map.py
@@ -1,27 +1,28 @@
 import js
 
+
 class Map:
     def __init__(self, X, Y):
         self.X = X
         self.Y = Y
 
     def drawMap(self):
-        '''
+        """
         html문서 내 X x Y 격자무늬 map을 그려주는 함수
-        '''
-        container = js.document.createElement('div')
-        container.setAttribute('class', 'map-container')
-        map_items = js.document.createElement('div')
-        map_items.setAttribute('class', 'map-items')
+        """
+        container = js.document.createElement("div")
+        container.setAttribute("class", "map-container")
+        map_items = js.document.createElement("div")
+        map_items.setAttribute("class", "map-items")
 
-        map_items.style.gridTemplateRows = f'repeat({self.X}, 10rem)'
-        map_items.style.gridTemplateColumns = f'repeat({self.Y}, 10rem)'
-        for i in range(0, self.X*self.Y):
-            item = js.document.createElement('div')
-            item.setAttribute('class', 'map-item')
+        map_items.style.gridTemplateRows = f"repeat({self.X}, 10rem)"
+        map_items.style.gridTemplateColumns = f"repeat({self.Y}, 10rem)"
+        for i in range(0, self.X * self.Y):
+            item = js.document.createElement("div")
+            item.setAttribute("class", "map-item")
             map_items.appendChild(item)
         container.appendChild(map_items)
-        
+
         # for i in range(0, self.X):
         #     flex = js.document.createElement('div')
         #     flex.setAttribute('class', 'map-flex')

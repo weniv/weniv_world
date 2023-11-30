dc10a1cd793b6272f66a158c624bf35ce2a48e82
baekg <baekg.dev@gmail.com>
2023-11-30 18:05:32 +0900

    :memo: Edit: 디자인 작업 후 적용을 위해 체력,마나 숨김 처리

--- assets/py/character.py
+++ assets/py/character.py
@@ -61,10 +61,10 @@ class Character:
         character.style.transition = f"all {running_speed}s"
         # character.style.width = f"{self.width}px"
         # character.style.height = f"{self.height}px"
-        hp = self.draw_hp()
-        character.appendChild(hp)
-        mp = self.draw_mp()
-        character.appendChild(mp)
+        # hp = self.draw_hp()
+        # character.appendChild(hp)
+        # mp = self.draw_mp()
+        # character.appendChild(mp)
         # next value(px) : (-1, -3), (-33, -1), (-65, -2), (-97, -3), (-129, -2), (-161, -1), (-193, -2)
         character.style.top = f"{self.x * 100 + 2 + (50 - 32)}px"
         character.style.left = f"{self.y * 100 + 2 + (50 - 32)}px"

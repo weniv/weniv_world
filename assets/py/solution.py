63b5e3d0c362a42b16366ee8fdf864b5a51a3d51
baekg <baekg.dev@gmail.com>
2023-12-21 09:18:34 +0900

    :sparkles: Feat: print, say 평가

---
+++ assets/py/solution.py
@@ -0,0 +1,14 @@
+story_solution = {
+    # 1번 스토리
+    1: {
+        'item_data':{},
+        'say_data':['hello world'],
+        'character_data':{
+            "character": "licat",
+            "x": 0,
+            "y": 0,
+            } ,
+        'item': {'fish-1':4},
+        
+    }
+}

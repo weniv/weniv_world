f3e743ab2236fb9fe18a1cb66b61604007159c7e
baekg <baekg.dev@gmail.com>
2023-12-22 13:45:20 +0900

    :memo: Add: 스토리 솔루션 추가 (6-11)

--- assets/py/solution.py
+++ assets/py/solution.py
@@ -36,5 +36,53 @@ story_solution = {
     },
     5: {
         "print_data": ["최고 운영 책임자 뮤라, 최고 기술 책임자 하티로 임명합니다. - 대표 라이캣"]
-    }
+    },
+    6: {
+        "print_data":["가장 적은 금액: 45000","이벤트 요일: 수","쉬는 날: 금"],
+        "item_data": {},
+    },
+    7: {
+        "say_data": ["오늘은 밥차를 운영합니다!"],
+        "item_data": {(1,4):{'item':'fish-1','count':1}},
+        "character_data": {
+            "character": "licat",
+            "x": 1,
+            "y": 4
+        },
+        "item":{'fish-1':11,'goldbar':15}
+    },
+    8: {
+        "print_data":["27"],
+        "item_data": {},
+    },
+    9: {
+        "say_data":['배송 1개 완료', '배송 3개 완료', '배송 1개 완료', '배송 5개 완료', '배송 7개 완료'],
+        "item_data": {},
+        "character_data": {
+            "character": "licat",
+            "x": 0,
+            "y": 4
+        },
+    },
+    10: {
+        "print_data" : ["골드바는 3개 있습니다. 물고기는 5마리 있습니다."],
+        "item_data":{},
+        "character_data" : {
+            "character": "licat",
+            "x":4,
+            "y":1
+        },
+        "item":{'fish-2': 1, 'goldbar': 3, 'fish-3': 2, 'fish-1': 2}
+    },
+    11: {
+        "print_data" : ["12"],
+        "item_data":{(4,4):{'item':'goldbar','count':1}},
+        "character_data" : {
+            "character": "licat",
+            "x":4,
+            "y":4
+        },
+        "item":{'diamond':12},
+        
+    },
 }

232a64f8555d291b2f0c3b5557f34093372a0cf9
Gyeonghyeon Baek <96777064+baekg110@users.noreply.github.com>
2023-12-22 16:28:12 +0900

    Revert "Revert "📝 PR: 스토리 모드 제출 기능 ""

---
+++ assets/py/solution.py
@@ -0,0 +1,88 @@
+story_solution = {
+    1: {
+        "say_data": ["hello, world!"],
+        "item_data": {},
+        "character_data": {
+            "character": "licat",
+            "x": 0,
+            "y": 0
+        },
+        "item": { "fish-1": 4 }
+    },
+    2: {
+        "say_data": ["문을 열어줘!"],
+        "item_data": {},
+        "character_data": {
+            "character": "licat",
+            "x": 2,
+            "y": 2
+        },
+        "item": { "diamond": 1 }
+    },
+    3: {
+        "print_data": ["라이캣은 물고기 6마리를 잡았다!"],
+        "item_data": {},
+        "character_data": {
+            "character": "licat",
+            "x": 0,
+            "y": 0
+        },
+        "item": { "fish-1": 6 }
+    },
+    4: {
+        "print_data": ["종류   마리    가격    합", "fish-1 2       1000   2000","fish-2 5       2000   10000","fish-3 10      3000   30000", "합                    42000"],
+        "item_data": {},
+        "item": {"fish-1":2, "fish-2":5, "fish-3":10}
+    },
+    5: {
+        "print_data": ["최고 운영 책임자 뮤라, 최고 기술 책임자 하티로 임명합니다. - 대표 라이캣"]
+    },
+    6: {
+        "print_data":["가장 적은 금액: 45000","이벤트 요일: 수","쉬는 날: 금"],
+        "item_data": {},
+    },
+    7: {
+        "say_data": ["오늘은 밥차를 운영합니다!"],
+        "item_data": {(1,4):{'item':'fish-1','count':1}},
+        "character_data": {
+            "character": "licat",
+            "x": 1,
+            "y": 4
+        },
+        "item":{'fish-1':11,'goldbar':15}
+    },
+    8: {
+        "print_data":["27"],
+        "item_data": {},
+    },
+    9: {
+        "say_data":['배송 1개 완료', '배송 3개 완료', '배송 1개 완료', '배송 5개 완료', '배송 7개 완료'],
+        "item_data": {},
+        "character_data": {
+            "character": "licat",
+            "x": 0,
+            "y": 4
+        },
+    },
+    10: {
+        "print_data" : ["골드바는 3개 있습니다. 물고기는 5마리 있습니다."],
+        "item_data":{},
+        "character_data" : {
+            "character": "licat",
+            "x":4,
+            "y":1
+        },
+        "item":{'fish-2': 1, 'goldbar': 3, 'fish-3': 2, 'fish-1': 2}
+    },
+    11: {
+        "print_data" : ["12"],
+        "item_data":{(4,4):{'item':'goldbar','count':1}},
+        "character_data" : {
+            "character": "licat",
+            "x":4,
+            "y":4
+        },
+        "item":{'diamond':12},
+        
+    },
+}

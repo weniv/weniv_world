285d3ec2ca4db7e68d6ebca0f5331d238c4b2b2c
baekg <baekg.dev@gmail.com>
2023-12-22 10:39:36 +0900

    :memo: Add: 스토리 5번까지 정답 조건 작성

--- assets/py/solution.py
+++ assets/py/solution.py
@@ -1,14 +1,40 @@
 story_solution = {
-    # 1번 스토리
     1: {
-        'item_data':{},
-        'say_data':['hello, world!'],
-        'character_data':{
+        "say_data": ["hello, world!"],
+        "item_data": {},
+        "character_data": {
             "character": "licat",
             "x": 0,
-            "y": 0,
-            } ,
-        'item': {'fish-1':4},
-        
+            "y": 0
+        },
+        "item": { "fish-1": 4 }
+    },
+    2: {
+        "say_data": ["문을 열어줘!"],
+        "item_data": {},
+        "character_data": {
+            "character": "licat",
+            "x": 2,
+            "y": 2
+        },
+        "item": { "diamond": 1 }
+    },
+    3: {
+        "print_data": ["라이캣은 물고기 6마리를 잡았다!"],
+        "item_data": {},
+        "character_data": {
+            "character": "licat",
+            "x": 0,
+            "y": 0
+        },
+        "item": { "fish-1": 6 }
+    },
+    4: {
+        "print_data": ["종류   마리    가격    합", "fish-1 2       1000   2000","fish-2 5       2000   10000","fish-3 10      3000   30000", "합                    42000"],
+        "item_data": {},
+        "item": {"fish-1":2, "fish-2":5, "fish-3":10}
+    },
+    5: {
+        "print_data": ["최고 운영 책임자 뮤라, 최고 기술 책임자 하티로 임명합니다. - 대표 라이캣"]
     }
 }
